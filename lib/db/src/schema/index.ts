686c0b4149143b6cb630720b6fa4982f72aee8f1
25flash <sdsnsck6mr@privaterelay.appleid.com>
2026-06-14 14:31:54 +0200

    Delete lib directory

--- lib/db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./users";
-export * from "./business-profiles";
-export * from "./cards";
-export * from "./scan-logs";
-export * from "./subscriptions";
-export * from "./review-replies";
-export * from "./support-messages";

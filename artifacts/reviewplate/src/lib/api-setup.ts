8a965622105e4c8e814ddc2113f6a47aca47447d
25flash <sdsnsck6mr@privaterelay.appleid.com>
2026-06-14 14:31:31 +0200

    Delete artifacts directory

--- artifacts/reviewplate/src/lib/api-setup.ts
+++
@@ -1,7 +0,0 @@
-import { setAuthTokenGetter } from "@workspace/api-client-react";
-
-export function initializeApiAuth() {
-  setAuthTokenGetter(() => {
-    return localStorage.getItem("reviewplate_token");
-  });
-}
